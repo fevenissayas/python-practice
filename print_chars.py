28a0889c9355efa78291134e30c69678bc9d2ab5
fevenissayas <feven.issayas-ug@aau.edu.et>
2023-07-08 15:41:31 +0300

    a python program that prints characters of a string

---
+++ print_chars.py
@@ -0,0 +1,9 @@
+def PrintChars(myString):
+    stringLength = len (str(myString))
+    for i in range(stringLength):
+        my_char = myString[i]
+        print(my_char)
+
+PrintChars("hi")
+PrintChars("1234")
+PrintChars("this is fun")
