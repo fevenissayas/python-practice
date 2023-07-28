d6298ac8e66f1fac85b9b57ff930105ffc452cf5
fevenissayas <feven.issayas-ug@aau.edu.et>
2023-07-28 16:08:12 +0300

    simple dict testing

---
+++ dict_practice.py
@@ -0,0 +1,19 @@
+x = [2,5,4,3,6,10,15]
+
+x.append (20)
+x.insert (1,35)
+print (x [3:6])
+
+
+y = {'one':1, 'two':2, 'three':3}
+#to append in a dictionary
+
+y ['four'] = 4
+
+#updating
+
+y ['two'] = 4 #thus, it's mutable
+
+for i in y:
+    print (y [i])
+    #print (i)
