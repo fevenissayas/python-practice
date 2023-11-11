01ee15519acd12b93fdfb36c64514a2ed5c5b9cd
fevenissayas <feven.issayas-ug@aau.edu.et>
2023-11-11 16:05:49 +0300

    area of circle

---
+++ circle_area.py
@@ -0,0 +1,14 @@
+pi = 3.14
+r = eval (input ("radius = "))
+def area (r):
+    print ("Area is:", pi * r **2)
+    
+    return area
+area (r)
+
+#how teacher did it
+
+def circle (r):
+    a = 3.14 * r**2
+    return a
+a = circle (5) #i forgot :)
